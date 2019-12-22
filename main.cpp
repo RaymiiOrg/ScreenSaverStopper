91f26166aa81582435fe545bde3948b81ba611c8
Remy <a@b.com>
2019-12-22 12:34:53 +0100

    Feedback from Lobste.rs. Build "gui" to not have to hide the console window. Initialize the input outside of the loop.
    Signed-off-by: Remy <a@b.com>

--- main.cpp
+++ main.cpp
@@ -8,20 +8,18 @@
 #include <windows.h>
 int main()
 {
-    // Hide this console window
-    ShowWindow(GetConsoleWindow(), SW_HIDE);
     // 40 seconds
     DWORD sleeptime = 40000;
     INPUT ip;
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wScan = 0; // hardware scan code for key
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
+    // list of keycodes:
+    // http://web.archive.org/web/20191221104344/https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+    ip.ki.wVk = 0x87; // virtual-key code for the "F24" key
     while (true)
     {
-        ip.type = INPUT_KEYBOARD;
-        ip.ki.wScan = 0; // hardware scan code for key
-        ip.ki.time = 0;
-        ip.ki.dwExtraInfo = 0;
-        // list of keycodes:
-        // http://web.archive.org/web/20191221104344/https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-        ip.ki.wVk = 0x87; // virtual-key code for the "F24" key
         ip.ki.dwFlags = 0; // 0 for key press
         SendInput(1, &ip, sizeof(INPUT));
         ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
